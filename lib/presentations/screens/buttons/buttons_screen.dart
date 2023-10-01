4a4c17ad0493c7ed0d22ce25749d6fec181bf40d
Yuri F. Marques <yuri.marques.dev@gmail.com>
2023-10-01 18:48:49 +0200

    124

--- lib/presentations/screens/buttons/buttons_screen.dart
+++ lib/presentations/screens/buttons/buttons_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ButtonsScreen extends StatelessWidget {
+  static const name = "buttons_screen";
+
   const ButtonsScreen({super.key});
 
   @override
