f89d29ca42175ecb81bad036eb0c18ede8dd944c
Yuri F. Marques <yuri.marques.dev@gmail.com>
2023-10-01 18:35:58 +0200

    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentations/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentations/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentations/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: AppTheme(selectedColor: 4).getTheme(),
-        debugShowCheckedModeBanner: false,
-        home: const HomeScreen(),
-        routes: {
-          "/buttons": (context) => const ButtonsScreen(),
-          "/cards": (context) => const CardsScreen(),
-        });
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: AppTheme(selectedColor: 4).getTheme(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
--- lib/presentations/screens/home/home_screen.dart
+++ lib/presentations/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentations/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -51,7 +51,9 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+
         // Navigator.of(context).push(MaterialPageRoute(
         //   builder: (context) => const ButtonsScreen(),
         // ));
