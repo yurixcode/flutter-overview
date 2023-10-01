72b47c130df733bca5df1316d4a5b9b6ba1b8d7e
Yuri F. Marques <yuri.marques.dev@gmail.com>
2023-10-01 16:52:59 +0200

    122

--- lib/presentations/screens/home/home_screen.dart
+++ lib/presentations/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentations/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -50,7 +51,10 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        // TODO: router
+        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.of(context).push(MaterialPageRoute(
+        //   builder: (context) => const ButtonsScreen(),
+        // ));
       },
     );
   }
