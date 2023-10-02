23a6a3754a2db83a161931a5b4338fff5ccfa94a
Yuri F. Marques <yuri.marques.dev@gmail.com>
2023-10-02 19:29:58 +0200

    130

--- lib/presentations/screens/screens.dart
+++ lib/presentations/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/presentations/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentations/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentations/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentations/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentations/screens/home/home_screen.dart';
+export 'package:widgets_app/presentations/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentations/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentations/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentations/screens/ui_controls/ui_controls_screen.dart';
