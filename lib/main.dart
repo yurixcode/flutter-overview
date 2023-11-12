a92828c90bc65d13ce9c4a83a84954317990c0ba
Yuri F. Marques <yuri.marques.dev@gmail.com>
2023-11-12 18:42:56 +0100

    * chore(main.dart): comment out unused variables isDarkMode and selectedColor

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final bool isDarkMode = ref.watch(isDarkModeProvider);
-    final int selectedColor = ref.watch(selectedColorProvider);
+    // final bool isDarkMode = ref.watch(isDarkModeProvider);
+    // final int selectedColor = ref.watch(selectedColorProvider);
 
     final AppTheme appTheme = ref.watch(themeNotifierProvider);
 
